2478a369b99286aec69649096db1a56b3ff5fcc7
Johathan29 <84540634+Johathan29@users.noreply.github.com>
2024-06-21 14:59:59 -0400

    Add files via upload

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { NgFor } from '@angular/common';
 import { PortafolioComponent } from '../portafolio/portafolio.component';
 import { ProyectosComponent } from '../proyectos/proyectos.component';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { Post } from '../post';
+import { HttpClient } from '@angular/common/http';
+import { NotificacionesService } from '../notificaciones.service';
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [PortafolioComponent, ProyectosComponent, FontAwesomeModule],
+  imports: [PortafolioComponent, ProyectosComponent, FontAwesomeModule, NgFor],
+  providers: [NotificacionesService],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.css',
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
   faCoffee = faArrowRight;
+  faBell = faBell;
+  notifi = 20;
+  valor: any;
+  datas = [
+    {
+      name: 'Data to enrich your online business',
+      description:
+        ' Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.',
+      url: '/',
+    },
+  ];
+  constructor(private resultadoPeticion: NotificacionesService) {}
+  ngOnInit() {
+    this.valor = this.datas.find(
+      (item) =>
+        (item.name = 'Data to enrich your online business' ? item.name : '')
+    );
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{html,ts,js}', './node_modules/flowbite/**/*.js'],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   plugins: [require('flowbite/plugin')],
