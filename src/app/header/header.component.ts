8b056490d73a00bbb22b027cfaa07cb1fa373758
Johathan29 <rosariojohathan@gmail.com>
2024-07-04 15:37:20 -0400

    cambios en header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,27 +1,37 @@
 import { Component, } from '@angular/core';
-import { NgFor,NgClass} from '@angular/common';
-import { RouterLink, RouterModule, ActivatedRoute } from '@angular/router';
+import { NgFor,NgClass,NgIf} from '@angular/common';
+import { RouterLink, RouterModule, ActivatedRoute, ChildrenOutletContexts } from '@angular/router';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FontAwesomeModule,NgFor,NgClass,RouterLink, RouterModule,   ],
+  imports: [FontAwesomeModule,NgFor,NgClass,NgIf,RouterLink, RouterModule,   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   faCoffee = faBell;
   navegation=[
-    
+    {
+titulo:'Portada',
+url:'/',
+children:[
+  {
+  childtitle:'',
+childurl:''
+}
+],
+
+
+  },
   {
     titulo:'Portafolio',
     url:'/portafolio',
-    children:[
-      {
+    children:[{
       childtitle:'Wordpress',
-      childurl:'wordpress'
+    childurl:'wordpress'
     },
     {
       childtitle:'Angular',
