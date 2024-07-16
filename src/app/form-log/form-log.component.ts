e2e76e1d7c35681ed33319a28ef6ad4c9200fc24
Johathan29 <rosariojohathan@gmail.com>
2024-07-16 08:39:22 -0400

    localstorage

--- src/app/form-log/form-log.component.ts
+++ src/app/form-log/form-log.component.ts
@@ -1,15 +1,48 @@
-import { Component } from '@angular/core';
-import { FormSubmittedEvent, FormsModule, NgForm, } from '@angular/forms';
-import { NgFor,JsonPipe } from '@angular/common';
+import { Component,OnInit,Injectable } from '@angular/core';
+import { FormSubmittedEvent, FormsModule, NgForm,FormBuilder } from '@angular/forms';
+import { NgFor,JsonPipe,NgClass } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-form-log',
   standalone: true,
-  imports: [FormsModule, NgFor,JsonPipe],
+  imports: [FormsModule, NgFor,JsonPipe,NgClass],
+
   templateUrl: './form-log.component.html',
   styleUrl: './form-log.component.css'
 })
-export class FormLogComponent {
+
+export class FormLogComponent implements OnInit { 
+  resultadoPeticion:any;
+   name:any;
+   classes=['max-w-sm', 'mx-auto', 'z-50', 'bg-[#19243ccc]', 'rounded-md', 'p-4']; 
+  asignaClases()
+  {
+   
+    
+    return this.classes;
+  }
+  constructor( private mibuilder : FormBuilder) { }
+  activo=false;
+  ngOnInit() { this.get();console.log(this.asignaClases()); this.name= localStorage.getItem("nombre");}
+  get() {
+
+  }
+    
   onSubmit(miForm: NgForm){
+    
+    if(miForm.value.email==='rosariojohathan@gmail.com' && miForm.value.password==='12345')
+    {
+      alert('hola Johathan');
+      this.activo=true;
+      this.classes.push('hidden');
+      console.log(this.asignaClases());
+   localStorage.setItem('nombre','Johathan');
+   
+    console.log(this.name);
+    } else{
+      alert('su email y password son incorretas, intenta de nuevo');
+    }
     console.log(miForm.value)
   }
+  
 }
