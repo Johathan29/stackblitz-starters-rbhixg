e58b3fe3a0cf7a1b147e39700fcf2aaef5a7234c
Johathan29 <rosariojohathan@gmail.com>
2024-07-16 15:51:10 -0400

    add api de login

--- src/app/form-log/form-log.component.ts
+++ src/app/form-log/form-log.component.ts
@@ -1,8 +1,9 @@
 import { Component,OnInit,Injectable,Inject } from '@angular/core';
 import { FormSubmittedEvent, FormsModule, NgForm,FormBuilder } from '@angular/forms';
 import { NgFor,JsonPipe,NgClass } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import {APIService} from '../api.service'
+import { HttpClient,HttpRequest } from '@angular/common/http';
+import {APIService} from '../api.service';
+import {datauser} from '../Datauser'
 @Component({
   selector: 'app-form-log',
   standalone: true,
@@ -11,31 +12,42 @@ providers:[Injectable,APIService],
   templateUrl: './form-log.component.html',
   styleUrl: './form-log.component.css'
 })
-
+@Injectable()
 export class FormLogComponent implements OnInit { 
 
- 
-   name:any;
+  name:any;
+   data:any;
    classes=['max-w-sm', 'mx-auto', 'z-50', 'bg-[#19243ccc]', 'rounded-md', 'p-4']; 
-  
+   result:any;
    asignaClases()
   {
    
     
     return this.classes;
   }
+  resultadoPeticion:any;
+  
   constructor() { }
- Get() {
-    
-    }
+ get= async () => {
+    const response = await fetch('https://dummyjson.com/users');
+    this.result= await response.json();
+    console.log(this.result.users);
+
+
+  
+ }
+
   activo=false;
   ngOnInit() { //this.Get();
-   
-    console.log(this.asignaClases()); this.name= localStorage.getItem("nombre");}
+    this.get();
+
+this.asignaClases(); 
+    this.name= localStorage.getItem("nombre");
+  }
  
     
   onSubmit(miForm: NgForm){
-    
+   
     if(miForm.value.email==='rosariojohathan@gmail.com' && miForm.value.password==='12345')
     {
       alert('hola Johathan');
@@ -44,11 +56,11 @@ export class FormLogComponent implements OnInit {
       console.log(this.asignaClases());
    localStorage.setItem('nombre','Alexander');
    
-    console.log(this.name);
+    console.log(this.result.users);
     } else{
       alert('su email y password son incorretas, intenta de nuevo');
     }
-    console.log(miForm.value)
+  
   }
   
 }
